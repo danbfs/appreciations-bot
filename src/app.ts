b1eec70fd175ffc73ba4c3cf43d0859e63a4ebe5
danbfs <danielbaldifs@gmail.com>
2023-06-10 17:11:01 +0200

    get

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,9 @@ import express from "express";
 const app = express();
 const port = 8080;
 
-app.post("/", async () => {});
+app.get("/", async (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
   return console.log(`Express is listening at http://localhost:${port}`);
